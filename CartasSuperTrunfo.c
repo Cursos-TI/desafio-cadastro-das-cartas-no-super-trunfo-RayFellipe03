29c830659a92cfd2462a77b279f7e873c818fa8d
Ray Fellipe <eo.rayfell@gmail.com>
2025-02-10 15:00:36 +0000

    variveisCorrigidas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,16 +2,19 @@
 
 int main() {
     // variáveis separadas para cada atributo da cidade.
-    char[15] StateName1;
-    char[20] CityName1;
-    char[3]  CardCode1;
-    int      Populacao1, Area1, Pib1, PontosTuristicos1;
-
+    char StateName1[15];
+    char CityName1[20];
+    char CardCode1[3];
+    int  Populacao1, Area1, Pib1, PontosTuristicos1;
 
     // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    printf("Olá! seja bem vindo ao SuperTrunfo da MateCheck!\n");
+
+    printf("para começar, nos informe os valores das suas cartas!\n");
+
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
