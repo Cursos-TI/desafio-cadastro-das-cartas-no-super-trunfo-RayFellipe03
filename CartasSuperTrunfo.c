fb523e1ceeceaa72814ebac1ccc775c0c0c8bd8e
Ray Fellipe <eo.rayfell@gmail.com>
2025-02-10 16:12:14 +0000

    SuperT Beta 0.1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,7 @@ int main() {
     char StateName1[15];
     char CityName1[20];
     char CityCode1[3];
-    int  PontosTuristicos1;
+    int TouristPoints1;
     float Area1, Populacao1, Pib1;
 
     // Cadastro das Cartas:
@@ -27,19 +27,25 @@ int main() {
     printf("\nQual a area da sua cidade?(Km²) ");
     scanf("%f", &Area1);
     printf("Quantos pontos turisticos possuem a sua cidade?");
-    scanf("%d", &PontosTuristicos1);
+    scanf("%d", &TouristPoints1);
     printf("\nQual a população da cidade? ");
     scanf("%f", &Populacao1);
     printf("\nQual o PIB da cidade? ");
     scanf("%f", &Pib1);
 
     // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     printf("\n-------------------------------------\n");
     printf("Certo, vamos conferir os dados das suas cartas:\n");
     // codigo, cidade(estado), populaçao, pib, area, turistico:
     printf("CODIGO: %s", CityCode1);
+    printf("\nCIDADE: %s", CityName1);
+    printf("(%s)", StateName1);
+    printf("\nPOPULAÇAO: %.2f", Populacao1);
+    printf("\nAREA: %.2f", Area1);
+    printf("\nPONTOS TURISTICOS: %d", TouristPoints1);
+    printf("\nPIB: %.2f", Pib1);
+
     printf("\n fim \n");
 
     return 0;
